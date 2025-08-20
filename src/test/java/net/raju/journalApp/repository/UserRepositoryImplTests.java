a10ae03301670e3a45a8893ea39f6ad866ce8205
raju <yesurajukadiyam143@gmail.com>
2025-08-20 19:58:29 +0530

    testDisabled

--- src/test/java/net/raju/journalApp/repository/UserRepositoryImplTests.java
+++ src/test/java/net/raju/journalApp/repository/UserRepositoryImplTests.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled
 @SpringBootTest
 public class UserRepositoryImplTests {
 
